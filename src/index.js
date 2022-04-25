39893a0d11540599cb40f2dc4bc818a027151300
Henrique Butzke Kotz <henriquebkotz@gmail.com>
2022-04-25 11:33:40 -0300

    basic features

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let users = [];
+let tweets = [];
+
+app.post('/sign-up', (req, res) => {
+    users.push(req.body);
+    res.send('OK');
+});
+
+app.post('/tweets', (req, res) => {
+    const { username, tweet } = req.body;
+    const user = users.find(elem => elem.username === username);
+    const newTweet = {
+        username,
+        avatar: user.avatar,
+        tweet
+    };
+    tweets.push(newTweet);
+    res.send('OK');
+});
+
+app.get('/tweets', (req, res) => {
+    const lastTweets = tweets.slice(-10);
+    res.send(lastTweets.reverse());
+});
+
 app.listen(5000, () => {
     console.log('Listening on port 5000...');
 })
